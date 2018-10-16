66f25a9266a65504b0c5fab5b785558fa3b87df9
sjholt83 <samueljholt001@gmail.com>
2018-10-16 15:18:24 -0400

    Refactoring for new request json

--- ddService.py
+++ ddService.py
@@ -1,41 +1,66 @@
 #!flask/bin/python
-from flask import Flask, jsonify, request, abort, make_response
+from flask import Flask, jsonify, request, make_response
+import json
 
 app = Flask(__name__)
 
 
-@app.route('/requestTransfer', methods=['POST'])
+@app.route('/api/requestTransfer', methods=['POST'])
 def process_request():
-    if not request.json:
-        abort(400)
+    #### Get json request data and store in an object for use
+    datapkg = {}
+    datapkg['GUID'] = request.json['GUID']
+    datapkg['priority'] = request.json['priority']
+    datapkg['transfertype'] = request.json['transfertype']
+    datapkg['destination'] = request.json['destination']
+    datapkg['username'] = request.json['username']
+    datapkg['password'] = request.json['password']
+    json_data = json.dumps(datapkg)
+    print("Incoming Request:")
+    print(json_data)
 
-    filefingerprint = request.json['guideid']
-    priority = request.json['priority']
-    token = request.json['oauthtoken']
-    destination = request.json['destination']
-    transfertype = request.json['transfertype']
-    if request.json['transforms']:
-        transforms = request.json['transforms']
+    #### Make Call to Security Service to obtain authorization
+    print("Call to Security Service:")
+    print(datapkg['GUID'], datapkg['destination'], datapkg['username'])
+
+#    r = request.post('https://123.456.789.001/api/proxy', external=True,
+#                     json_data)
+
+    #### Get response from Security Service
+    securityResponse = {}
+    securityResponse['username'] = datapkg['username']
+    securityResponse['GUID'] = datapkg['GUID']
+    securityResponse['authorization'] = 'authorized'
+    securityResponse['filelocation'] = 'Garage'
+    response_data = json.dumps(securityResponse)
+    print("Response from Security Service:")
+    print(response_data)
+
+    #### Perform logic on response to see if we continue
+    print("Check Authorization:")
+    if securityResponse['authorization'] != "authorized":
+        print("User is not Authorized")
+        return("status: " + "500")
     else:
-        transforms = ""
+        print("User is Authorized:")
+        #### Make call to transfer service for file transfer
+        transferRequest = {}
+        transferRequest['fileuri'] = securityResponse['filelocation']
+        transferRequest['destination'] = datapkg['destination']
+        transferRequest['GUID'] = securityResponse['GUID']
+        transferRequest['username'] = securityResponse['username']
+        transferRequest['password'] = datapkg['password']
+        request_data = json.dumps(transferRequest)
+        print("Request for Transfer Service:")
+        print(request_data)
 
-    r = request.post('https://123.456.789.001/api/proxy', external=True,
-                     data={'token': token, 'file': filefingerprint})
-    if r.status_code == 200:
-        jsonify(r.text)
-        if r.json['authorization'] == 'True':
-            authorization = True
-            fileuri = r.json['fileuri']
-            xfer = request.post('https://123.456.789.001/api/' + transfertype, data={'destination': destination,
-                                                                                  'fileuri': fileuri, 'priority': priority,
-                                                                                  'oauth': token, 'transforms': transforms})
-        else:
-            authorization = False
-    else:
-        authorization = False
+        #### Mock call to Transfer Service
+#       xfer = request.post('https://123.456.789.001/api/' + transfertype, request_data)
 
-    return jsonify({'request status code': r.status_code, 'description': r.reason, 'authorization': authorization}), 200
 
+#   return jsonify({'request status code': r.status_code, 'description': r.reason, 'authorization': authorization}), 200
+
+    return(request_data)
 
 @app.errorhandler(404)
 def not_found(error):
@@ -44,3 +69,5 @@ def not_found(error):
 
 if __name__ == '__main__':
     app.run(threaded=True, debug=True, port=5000)
+
+
